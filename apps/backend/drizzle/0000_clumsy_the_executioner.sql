fb543dbfe55cdd85fb74b86db261fc91050e3649
iiivanpopov <iiivanpopov999@gmail.com>
2025-10-21 20:51:44 +0300

    chore: add migrations to git; build(front): dont check types on prod

---
+++ apps/backend/drizzle/0000_clumsy_the_executioner.sql
@@ -0,0 +1,42 @@
+CREATE TYPE "public"."roles" AS ENUM('user', 'admin');--> statement-breakpoint
+CREATE TABLE "repairs" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "repairs_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"user_id" integer NOT NULL,
+	"pcb_name" varchar(255) NOT NULL,
+	"defect" varchar(255) NOT NULL,
+	"note" text,
+	"date" timestamp with time zone NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE "test_result" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "test_result_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"user_id" integer NOT NULL,
+	"pcb_name" varchar(255) NOT NULL,
+	"passed_first_try" integer NOT NULL,
+	"failed" integer NOT NULL,
+	"total" integer NOT NULL,
+	"date" timestamp with time zone NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE "tokens" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "tokens_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"user_id" integer NOT NULL,
+	"token" text NOT NULL,
+	CONSTRAINT "tokens_user_id_unique" UNIQUE("user_id")
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"full_name" varchar(100) NOT NULL,
+	"user_name" varchar(30) NOT NULL,
+	"role" "roles" DEFAULT 'user' NOT NULL,
+	"password_hash" text NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now(),
+	CONSTRAINT "users_user_name_unique" UNIQUE("user_name")
+);
+--> statement-breakpoint
+ALTER TABLE "repairs" ADD CONSTRAINT "repairs_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "test_result" ADD CONSTRAINT "test_result_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "tokens" ADD CONSTRAINT "tokens_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default antfu({
   jsx: {
     a11y: true,
   },
+  ignores: ['**/drizzle'],
 }, {
   files: ['apps/backend/**/*'],
   rules: {
