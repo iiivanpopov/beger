fec285ba2f36991f223283babeaf42a28926cacc
iiivanpopov <iiivanpopov999@gmail.com>
2025-11-17 14:21:50 +0200

    move route search schema outside

--- apps/frontend/src/routes/(admin)/users.tsx
+++ apps/frontend/src/routes/(admin)/users.tsx
@@ -2,13 +2,18 @@ import { createFileRoute } from '@tanstack/react-router'
 import * as v from 'valibot'
 import { getUsersQueryOptions } from '@/api'
 import { UsersPage } from '@/pages/Users/UsersPage'
+import { queryClient } from '@/providers'
 import { requireRole } from '@/shared/utils'
+
+export const RouteSearchSchema = v.object({
+  page: v.optional(v.fallback(v.pipe(v.number(), v.toMinValue(1)), 1), 1),
+})
 
 export const Route = createFileRoute('/(admin)/users')({
   beforeLoad: ({ context }) => requireRole(context.role, ['admin'], '/login'),
   component: UsersPage,
-  validateSearch: v.object({ page: v.optional(v.fallback(v.pipe(v.number(), v.toMinValue(1)), 1), 1) }),
-  loader: ({ context, location }) => context.queryClient.ensureQueryData(getUsersQueryOptions({
+  validateSearch: RouteSearchSchema,
+  loader: ({ location }) => queryClient.ensureQueryData(getUsersQueryOptions({
     query: { limit: 5, page: 1, ...location.search },
-  })), // ...location.search instead of location.search.page cuz typescript
+  })),
 })
